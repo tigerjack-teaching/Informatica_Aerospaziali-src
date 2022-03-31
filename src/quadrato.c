24f18cf42bc01388fb48dba753135120fe42af8b
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2022-03-31 16:59:49 +0200

    Mod dir structure

--- src/quadrato.c
+++ src/quadrato.c
@@ -7,15 +7,15 @@ int main(int argc, char *argv[]) {
   printf("Inserisci un numero\n");
   scanf("%u", &n);
 
-  if (n <= 0) {
+  do {
     printf("Il numero inserito non è maggiore di 0");
     return 0;
-  }
+  } while (n <= 0);
 
   // Procediamo con il sommare i primi n numeri dispari, per calcolare il
-  // quadrato del numero n. Praticamente, quello che vogliamo fare è, facendo
-  // iterare i da 0 a n - 1, elencare tutti i numeri dispari usando la forma:
-  // 2*i + 1 == i + i + 1, e accumularli nel nostro accumulatore.
+  // quadrato del numero n. Praticamente, quello che vogliamo fare è,
+  // facendo iterare i da 0 a n - 1, elencare tutti i numeri dispari usando
+  // la forma: 2*i + 1 == i + i + 1, e accumularli nel nostro accumulatore.
   unsigned accum = 0;
   unsigned i = 0;
 
