34b91ec1cf58da4b6eced6ef741a254c2f16429f
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2022-11-09 14:28:06 +0100

    MOD selection sort description

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -3,6 +3,27 @@
  *
  * Implementare un programma che preso un vettore di interi, ordini i suoi
  * elementi in ordine crescente, utilizzando la tecnica dell'insertion sort.
+ *
+ * ESEMPIO:
+ *
+ * STEP 1: arr[] = {12, _11_, 13, 5, 6}
+ *   - comparo 11 con 12 -> posizioni sbagliate -> swap
+ *   - stop (non ci sono altre posizioni precedenti)
+
+ * STEP 2: arr[] = {11 | _12_, 13, 5, 6}
+ *   - comparo 13 con 12 -> ok -> stop -> {11, 12| 13, 5, 6}
+ *
+ * STEP 3: arr[] = {11, 12| _13_, 5, 6}
+ *   - comparo 5 con 13 -> swap -> {11, 12| 5, 13, 6}
+ *   - comparo 5 con 12 -> swap -> {11, 5, 12| 13, 6}
+ *   - comparo 5 con 11 -> swap -> {5, 11, 12| 13, 6}
+ *   - stop
+ *
+ * STEP 4: arr[] = {5, 11, 12| 13 _6_}
+ *   - comparo 6 con 13 -> swap -> {5, 11, 12| _6_, 13}
+ *   - comparo 6 con 12 -> swap -> {5, 11, _6_, 12| 13}
+ *   - comparo 6 con 11 -> swap -> {5, _6_, 11, 12| 13}
+ *   - comparo 6 con 5 -> no swap -> stop
  */
 #include <stdio.h>
 #define LUN_MAX 100
